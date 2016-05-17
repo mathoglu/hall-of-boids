fb77a414fde40987b10419806d6ea2b1a94cee24
Matts Höglund <matts.hoglund@netlight.com>
2016-05-17 14:15:07 +0300

    new NL logo

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,6 +3,6 @@ import {App} from './app';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import { HTTP_PROVIDERS } from "angular2/http";
 import '../styles/main.scss';
-import '../assets/images/EPS_RGB_WHITE_10.png'
+import '../assets/images/nl-logo.png'
 
 bootstrap(App, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
--- src/widget/main.ts
+++ src/widget/main.ts
@@ -3,6 +3,6 @@ import {WidgetApp} from './app';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import { HTTP_PROVIDERS } from "angular2/http";
 import '../styles/main.scss';
-import '../assets/images/EPS_RGB_WHITE_10.png'
+import '../assets/images/nl-logo.png'
 
 bootstrap(WidgetApp, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
