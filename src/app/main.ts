090717bb1313a8319264b72fc4ce2bce03aa1a0f
Matts Höglund <matts.hoglund@netlight.com>
2016-05-20 10:57:29 +0300

    Added polyfill initialisation to fix mobile safari date parsing bug

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,4 +5,9 @@ import { HTTP_PROVIDERS } from "angular2/http";
 import '../styles/main.scss';
 import '../assets/images/nl-logo.png'
 
+if (!global.Intl) {
+  require('intl');
+  require('intl/locale-data/jsonp/en.js');
+}
+
 bootstrap(App, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
